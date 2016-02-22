87e86c4f95ce3831142cf0b7f0af3700e450fe4f
zhehua_xiao <zhehuaxiao@gmail.com>
2016-02-22 23:14:11 +0800

    Move onTouch response code to dispatchTouchEvent().

--- uilibrary/src/main/java/zchang/me/uilibrary/BounceList.java
+++ uilibrary/src/main/java/zchang/me/uilibrary/BounceList.java
@@ -19,9 +19,11 @@ import android.content.Context;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Interpolator;
 
 /**
  *
@@ -36,36 +38,32 @@ public class BounceList extends RecyclerView {
 
 	float deltaY = 0;
 	float amountY = 0;
-	boolean isMoving = false;
 	int courseCount = 0;
 
 	@Override
-	public boolean onTouchEvent(MotionEvent motionEvent) {
+	public boolean dispatchTouchEvent(MotionEvent motionEvent) {
+		Log.i(TAG, "Current motion is " + motionEvent.getActionMasked());
 		switch (motionEvent.getActionMasked()) {
 			case MotionEvent.ACTION_DOWN:
 				//Log.i(TAG, "ACTION DOWN");
-				isMoving = false;
+				courseCount = getChildCount();
 				break;
 			case MotionEvent.ACTION_MOVE:
 				//Log.i(TAG, "ACTION MOVE");
 				if (((LinearLayoutManager) getLayoutManager()).findLastCompletelyVisibleItemPosition() == getAdapter().getItemCount() - 1
 						|| ((LinearLayoutManager) getLayoutManager()).findFirstCompletelyVisibleItemPosition() == 0) {
-					if (!isMoving) {
-						// start to move
-						isMoving = true;
-						courseCount = getChildCount();
-					} else if (motionEvent.getHistorySize() > 0) {
+					if (motionEvent.getHistorySize() > 0) {
 						// is moving
 						// add deltaY to amountY
 						deltaY = motionEvent.getY(motionEvent.getActionIndex())
 								- motionEvent.getHistoricalY(motionEvent.getActionIndex(), motionEvent.getHistorySize() - 1);
+						amountY += deltaY;
 						for (int i = 1; i < courseCount; i++) {
 							View childView = getChildAt(i);
-							amountY = childView.getTranslationY();
-							int position = childView.getTranslationY() > 0 ? i : courseCount - i;
-							//float deltaYi =
-							//		 ((position / 6.f) * (float)Math.atan(amountY / getHeight() * 10f) * getHeight() / 1.6f);
-							childView.setTranslationY(amountY < getHeight() / 2 ? amountY + deltaY / 6f * (1 + position * .5f) : amountY);
+							int position = amountY > 0 ? i : courseCount - i;
+							float deltaYi =
+									((position / 6.f) * (float) Math.atan(amountY / getHeight() * 10f) * getHeight() / 1.6f);
+							childView.setTranslationY(deltaYi / 6.f);
 						}
 					}
 				}
@@ -73,20 +71,19 @@ public class BounceList extends RecyclerView {
 			case MotionEvent.ACTION_CANCEL:
 			case MotionEvent.ACTION_UP:
 				//Log.i(TAG, "ACTION UP");
-				if (isMoving) {
-					for (int i = 1; i < courseCount; i++) {
-						getChildAt(i)
-								.animate()
-								.translationY(0)
-								.setInterpolator(new AccelerateDecelerateInterpolator())
-								.setDuration(180)
-								.setStartDelay(50);
-					}
+				amountY = 0;
+				Interpolator interpolator = new AccelerateDecelerateInterpolator();
+				for (int i = 1; i < courseCount; i++) {
+					getChildAt(i)
+							.animate()
+							.translationY(0)
+							.setInterpolator(interpolator)
+							.setDuration(180)
+							.setStartDelay(50);
 				}
-				isMoving = false;
 				break;
 		}
-		return super.onTouchEvent(motionEvent);
+		return super.dispatchTouchEvent(motionEvent);
 	}
 
 	@Override
